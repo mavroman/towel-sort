ab8f8a378e6e210314ef6c069a20b097790be436
Roman Mavrin <roma-mrv@yandex.ru>
2021-02-09 12:31:41 +0300

    docs: update towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return ((a[0]+',' + a[1].reverse() + ',' + a[2]).split(',')).map(e => parseFloat(e))
+  return ((matrix[0]+',' + matrix[1].reverse() + ',' + matrix[2]).split(',')).map(e => parseFloat(e))
 }
