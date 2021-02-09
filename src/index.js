64d311a08d699126656c2241b9f24d4f11ce221f
Roman Mavrin <roma-mrv@yandex.ru>
2021-02-09 15:26:08 +0300

    docs: update towel-sort

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+  if(matrix.length === 0) {
+    return []
+  } else {
+    
+  }
   for(let i = 0; i < matrix.length; i++) {
     if(i % 2) matrix[i].reverse()
   }
